56dbfcca8acad75578ccf6968646c969d96d3c57
Caio <caiovi98@hotmail.com>
2025-03-10 09:33:05 -0300

    Ajusta porta

--- server.js
+++ server.js
@@ -5,4 +5,4 @@ app.get("/", (req, res) => res.send("Hello, World! - Server is running"));
 
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
+app.listen(PORT, () => console.log(`Servidor rodando na porta: ${PORT}`));
