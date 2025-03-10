1040dda1838eb3f695df6ebfb9c51c61be8afcab
Caio <caiovi98@hotmail.com>
2025-03-10 08:55:30 -0300

    add package json

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
 const express = require("express");
-
 const app = express();
-const PORT = 3000;
 
-app.get("/hello", (req, res) => {
-  res.send("Hello, World! - Server is running");
-});
+app.get("/", (req, res) => res.send("Hello, World! - Server is running"));
 
-app.listen(PORT, () => {
-  console.log(`🚀 Servidor rodando em http://localhost:${PORT}`);
-});
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
